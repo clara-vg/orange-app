880ef539593b14786cc783d5de123aa6b5b00669
claravg <claravazquezg99@gmail.com>
2022-07-26 17:21:32 -0600

    catalogo con maps y promises

--- src/asyncMock.jsx
+++ src/asyncMock.jsx
@@ -4,7 +4,7 @@ export const products = [
         name: "Caminos",
         price: 100,
         category: "cuadros",
-        img: "https://i.pinimg.com/564x/56/e6/c3/56e6c3c1e7be5aac8d524a7dc047d90e.jpg",
+        img: "./images/cuadro1.png",
         stock: 5,
         description: "Descripcion de esta imagen"
     },
@@ -13,7 +13,7 @@ export const products = [
         name: "Senderos",
         price: 150,
         category: "cuadros",
-        img: "https://i.pinimg.com/564x/af/5c/c7/af5cc7976e6ed4f6a709d0b23007e66c.jpg",
+        img: "./images/cuadro2.png",
         stock: "5",
         description: "Descripcion de esta imagen"
     },
@@ -22,7 +22,7 @@ export const products = [
         name: "Relieves",
         price: 175,
         category: "cuadros",
-        img: "https://i.pinimg.com/564x/52/d5/ad/52d5ad9ea3113a8a6dd8bebbfdb113e2.jpg",
+        img: "./images/cuadro3.png",
         stock: "5",
         description: "Descripcion de esta imagen"
     }
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,6 +1,23 @@
 const Item = ( {product} ) => {
     return (
-       <li key={product.id}> {product.name} </li>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+        <li key={product.id}> {product.category} </li>
+            <img className="w-full" src= {product.img} alt="" />
+        <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2"> {product.name} </div>
+                <p className="text-gray-700 text-base">
+                    {product.description}
+                </p>
+            </div>
+        <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> <b> ${product.price}</b> </span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">  <b> Stock: {product.stock} </b></span>
+            <button className="bg-gray-200 hover:bg-white text-gray-700 font-bold py-2 px-4 rounded-full">
+                Ver detalle
+            </button>
+        </div>
+    </div>
+
     )   
    }
 
